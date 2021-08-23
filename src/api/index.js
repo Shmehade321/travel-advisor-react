7e62360ca6b5e1351284e5c3430807d32f52b7e2
Mehade Hasan <hasanmehade300@gmail.com>
2021-08-23 17:22:32 -0400

    Initial project commit

---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const URL =
+  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+          "x-rapidapi-key": "4cb321199dmsh5d4136f20a248ccp1b6617jsn1264e2308a84",
+        },
+      });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
