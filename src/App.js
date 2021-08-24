2be1545d27f5db24b5818088a16f8713bc7d2a31
Mehade Hasan <hasanmehade300@gmail.com>
2021-08-23 22:29:58 -0400

    Map scrolling issue WIP

--- src/App.js
+++ src/App.js
@@ -11,21 +11,27 @@ const App = () => {
   const [places, setPlaces] = useState([]);
 
   const [coordinates, setCoordinates] = useState({});
-  const [bounds, setBounds] = useState(null);
+  const [bounds, setBounds] = useState({});
+
+  const [childClicked, setChildClicked] = useState(null);
+
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
-      setCoordinates({lat: latitude, lng: longitude})
-    })
-  }, [])
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
 
   useEffect(() => {
-    console.log(coordinates, bounds)
-    // getPlacesData(bounds.sw, bounds.ne)
-    // .then(data => {
-    //   setPlaces(data)
-    //   console.log(data)
-    // })
+    setIsLoading(true);
+
+    // getPlacesData(bounds.sw, bounds.ne).then((data) => {
+    //   setPlaces(data);
+    //   setIsLoading(false)
+    // });
   }, [coordinates, bounds]);
 
   return (
@@ -34,13 +40,19 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <List places={places} />
+          <List
+            places={places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+          />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={places}
+            setChildClicked={setChildClicked}
           />
         </Grid>
       </Grid>
